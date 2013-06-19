3bf670f4cc498aea273cfa6a304c5ecfa2538a7d
kf <7kfpun@gmail.com>
2013-06-19 12:15:08 +0800

    update pip completion

--- bash_completion/pycompletion/pip-completion.bash
+++ bash_completion/pycompletion/pip-completion.bash
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-_pip_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   PIP_AUTO_COMPLETE=1 $1 ) )
-} &&
-complete -o default -F _pip_completion pip
+_pip() {
+
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    commands=$(pip --help | grep -E -o "^\s{2}\w*\:" | tr -d ' ' | tr -d ':')
+    #opts=$(pip --help | grep -P -o "((-\w{1}|--[\w-]*=?)){1,2}")
+    opts=$(pip --help | grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+
+
+    if [ $COMP_CWORD == 1 ] ; then
+      COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+      return 0
+    fi
+
+    if [[ ${cur} == -* ]] ; then
+        local command_opts=$(pip $prev --help | grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+        COMPREPLY=( $(compgen -W "${command_opts}" -- ${cur}) )
+        return 0
+    fi
+}
+
+complete -F _pip pip
+
+# /* vim: set filetype=sh : */
+
