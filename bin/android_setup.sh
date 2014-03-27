704a4113c565b17e29eef6c412af3a07e8885cec
kf <7kfpun@gmail.com>
2014-03-28 00:16:58 +0800

    add install gradle

--- bin/android_setup.sh
+++ bin/android_setup.sh
@@ -2,14 +2,12 @@
 ## Usage: Install android
 ##
 
-android=adt-bundle-linux-x86_64-20130514
+#android=adt-bundle-linux-x86_64-20130514
 
-rm -rf ~/android
-wget http://dl.google.com/android/adt/$android.zip -P /tmp/
-unzip /tmp/$android.zip -d /tmp/
-mv /tmp/$android ~/android
-
+#rm -rf ~/android
+#wget http://dl.google.com/android/adt/$android.zip -P /tmp/
+#unzip /tmp/$android.zip -d /tmp/
+#mv /tmp/$android ~/android
 
 # 32bit shared libraries for Android
-sudo apt-get install -y ia32-libs openjdk-7-jdk 
-
+sudo apt-get install -y ia32-libs openjdk-7-jdk gradle
