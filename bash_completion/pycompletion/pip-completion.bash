63f59468493af099ab9f43d0d653c3389a6fa6ce
kf <7kfpun@gmail.com>
2014-08-11 11:18:49 +0800

    update pip completion

--- bash_completion/pycompletion/pip-completion.bash
+++ bash_completion/pycompletion/pip-completion.bash
@@ -1,28 +1,30 @@
 #!/bin/sh
 
 _pip() {
-
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
+    first="${COMP_WORDS[0]}"
 
-    commands=$(pip --help | grep -E -o "^\s{2}\w*\:" | tr -d ' ' | tr -d ':')
-    #opts=$(pip --help | grep -P -o "((-\w{1}|--[\w-]*=?)){1,2}")
-    opts=$(pip --help | grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+    commands=$($first --help | awk '/Commands\:/,/General Options\:/' | \
+               \grep -E -o "^\s{2}\w*" | tr -d ' ')
+    opts=$($first --help | \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
 
 
     if [ $COMP_CWORD == 1 ] ; then
-      COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
-      return 0
+        COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+        return 0
     fi
 
     if [[ ${cur} == -* ]] ; then
-        local command_opts=$(pip $prev --help | grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+        local command_opts=$($first $prev --help | \
+                             \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
         COMPREPLY=( $(compgen -W "${command_opts}" -- ${cur}) )
         return 0
     fi
 }
 
-complete -F _pip pip
+complete -o default -F _pip pip
+complete -o default -F _pip pip2
+complete -o default -F _pip pip3
 
 # /* vim: set filetype=sh : */
-
