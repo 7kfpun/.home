4b2ef0c686b2a2abfb6c88bb423c08e81e64ead0
kf <7kfpun@gmail.com>
2014-12-04 15:52:03 +0800

    update home setup

--- bin/home_update.sh
+++ bin/home_update.sh
@@ -21,7 +21,7 @@ update_system () {
 
 install_home_tools () {
     print 'Install home tools.'
-    apt-get install -y guake skype xclip openvpn curl vim-nox
+    apt-get install -y guake skype xclip openvpn curl vim-nox ubuntu-restricted-addons ubuntu-restricted-extras
 
     # print "Install Vagrant and VirtualBox"
     # dpkg -i debs/*
@@ -47,11 +47,14 @@ install_home_tools () {
 
     # cd ~/.vim && git pull
     # cd ~/.home && git pull
+
+    apt-get install -y rhino
 }
 
 install_dev_tools () {
     print 'Install dev tools.'
-    apt-get install -y build-essential libxml2-dev git-core git-buildpackage exuberant-ctags python-software-properties subversion screen cmatrix virtualbox-qt openjdk-7-jdk curl nfs-kernel-server mongodb-clients redis-tools
+    apt-get install -y build-essential libxml2-dev git-core git-buildpackage exuberant-ctags python-software-properties curl nfs-kernel-server
+    # subversion screen cmatrix virtualbox-qt openjdk-7-jdk curl nfs-kernel-server mongodb-clients redis-tools
     apt-get install -y python-pip python-dev python-setuptools ipython
     apt-get install -y python3-pip python3-dev ipython3
     # apt-get install -y supervisor postgresql nodejs python-psycopg2
@@ -72,7 +75,7 @@ install_ec2_tools () {
 install_python_modules () {
     print 'Install python modules.'
     easy_install -U setuptools
-    for I in pip virtualenv virtualenv-clone ipython ipdb pylint httpie
+    for I in pip virtualenv virtualenv-clone ipython ipdb pylint httpie pip-tools
     do
         (python -c "import $I" 2>/dev/null || {
         echo -e ${red}$I${cyan} not found! Attempting to install... $nc;
@@ -90,8 +93,20 @@ install_grep_tool () {
     make install
 }
 
+install_node () {
+    echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+    . ~/.bashrc
+
+    cd /tmp
+    curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+    ./configure --prefix=~/local
+    make install # ok, fine, this step probably takes more than 30 seconds...
+    curl https://www.npmjs.org/install.sh | sh
+
+    sudo chown -R $USER /usr/local
+}   
+
 install_ruby () {
-    apt-get install nodejs nodejs-dev npm
 
     apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
 
@@ -100,8 +115,8 @@ install_ruby () {
     curl -L https://get.rvm.io | bash -s stable
     source ~/.rvm/scripts/rvm
     echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
-    rvm install 2.1.2
-    rvm use 2.1.2 --default
+    rvm install 2.1.5
+    rvm use 2.1.5 --default
     ruby -v
 
     # tell Rubygems not to install the documentation for each package locally
@@ -113,14 +128,14 @@ install_goagent () {
 }
 
 setup () {
-    #configure_locales
-    #update_system
-    #install_home_tools
+    configure_locales
+    update_system
+    install_home_tools
     install_dev_tools
     #install_ec2_tools
     install_python_modules
     install_grep_tool
-    install_ruby
+    #install_ruby
     #install_goagent
 }
 
